d3a72f39ea299fce9d8d83017b8110c643eb2d8a
scremin <azflresc@hotmail.com>
2020-07-01 14:32:18 +0100

    Acrescimo de informacao ao arquivo gestorMain.py

--- projeto1/TRECHO2/gestorMain.py
+++ projeto1/TRECHO2/gestorMain.py
@@ -1,13 +1,20 @@
 from mochila.bolso1 import capturador, decepamentoS1, decepamentoS2, separS2
 
 def captura(s_url, s_IDbuscado):
+    '''
+        A captura recolhe as informações localizadas numa posição específica
+        de cada endereço.
+    '''
     s = capturador(s_url, s_IDbuscado)
     return s
 
 
 def decepamento(s1, s2):
     '''
-        Decepamento.
+        Decepamento. As strings recebidas são recortadas/decepadas de acordo
+        com as características do site e das informações úteis.
+        São retornados dois dicionários com a primeira posição preenchida
+        com uma lista de listas.
     '''
     dic_s1 = decepamentoS1(s1)
     #
